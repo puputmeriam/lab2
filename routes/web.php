6604deb6ea5b6e5005d9c44424f6f2a9e4976e4b
puputmeriam <puputmaryam@gmail.com>
2022-02-02 14:16:45 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BiodataController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/biodata', function () {
+    $data=[
+        'nama'=>'Puput Maryam Rahmawati',
+        'lahir'=>'Garut, 09 Oktober 2003',
+        'hobi'=>'Berenang',
+        'jk'=>'Perempuan',
+        'agama'=>'Islam',
+        'alamat'=>'Komp. Bumi orange blok E7 no 19',
+        'telp'=>'089647062396',
+        'email'=>'puputmaryam03@gmail.com',
+    ];
+    return view('biodata',$data);
+});
+Route::get('/list', [BiodataController::class, 'index'])->name('list');
+
+Route::get('/form_siswa', [BiodataController::class, 'create'])->name('form_siswa');
+Route::post('/store_siswa', [BiodataController::class, 'store'])->name('store_siswa');
+Route::get('/edit/{id}', [BiodataController::class, 'edit'])->name('edit_siswa');
+Route::put('/update/{id}', [BiodataController::class, 'update'])->name('update_siswa');
+Route::get('/bio/{id}', [BiodataController::class, 'show'])->name('show_bio');
+Route::delete('/delete/{id}', [BiodataController::class, 'destroy'])->name('destroy-siswa');
